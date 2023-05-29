17d62331f2f09ba21df533d8b72609d0e302ee43
Lady-Amazon <hobby_finne@hotmail.com>
2023-05-29 15:22:42 +0200

    Updated Post with Created and Updated, added migration

---
+++ FutureHub.Core/Data/Migrations/20230529131354_UpdatedPost.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_FutureHub.Data.Migrations
+{
+    public partial class UpdatedPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Posts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Posts",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Posts");
+        }
+    }
+}
--- FutureHub.Shared/Models/Post.cs
+++ FutureHub.Shared/Models/Post.cs
@@ -9,22 +9,34 @@ public class Post
     public string Title { get; set; } = string.Empty;
     [Required]
     public string Content { get; set; } = string.Empty;
-   
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
     public string? AuthorId { get; set; }
     public ApplicationUser? Author { get; set; }
 
-    public Post()  { }
+    public Post() { }
 
-    public static Post Create(Guid id, string title, string content, string authorId, ApplicationUser author) /*, ApplicationUser author*/
+    public static Post Create(Guid id, string title, string content, string authorId, ApplicationUser author)
     {
-        var post = new Post() 
-        { 
-            Id = id, 
-            Title = title, 
-            Content = content, 
+        var post = new Post()
+        {
+            Id = id,
+            Title = title,
+            Content = content,
             AuthorId = authorId,
+            Created = DateTime.Now,
             Author = author
         };
         return post;
     }
+    //public static Post Update(string title, string content) 
+    //{
+    //    var post = new Post()
+    //    {
+    //        Title = title,
+    //        Content = content,
+    //        Updated = DateTime.Now
+    //    };
+    //    return post;
+    //}
 }
